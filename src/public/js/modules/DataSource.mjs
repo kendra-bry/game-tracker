a0bb2649e3c78a635b53d17b9884448b2d76b02f
Kendra Bryant <kbryant@efilecabinet.com>
2023-05-20 20:35:59 -0600

    Added to vite config

---
+++ src/public/js/modules/DataSource.mjs
@@ -0,0 +1,23 @@
+const baseURL = import.meta.env.VITE_API_URL;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+async function convertToJson(res) {
+  let jsonResponse = await res.json();
+  if (res.ok) {
+    return jsonResponse;
+  } else {
+    throw { name: 'servicesError', message: jsonResponse };
+  }
+}
+
+export default class DataSource {
+  constructor() {}
+
+  async getData() {
+    const data = await fetch(`${baseURL}/games?key=${apiKey}`);
+    const json = await convertToJson(data);
+    console.log({json});
+    return json.results;
+  }
+
+}
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,10 @@ export default defineConfig({
     outDir: '../dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html')
+        main: resolve(__dirname, 'src/index.html'),
+        details: resolve(__dirname, 'src/details/index.html'),
+        library: resolve(__dirname, 'src/library/index.html'),
+        search: resolve(__dirname, 'src/search/index.html'),
       },
     },
   },
